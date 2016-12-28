642dcc58020b5bb2d6d6b8421094ba541e336929
Airubby <ai_rubby@sina.cn>
2016-12-28 15:01:43 +0800

    增加音乐

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const path = require('path');
 const url = require('url');
 const _ = require('underscore');
+const qs = require('querystring');
 
 
 const musicList = [{
@@ -41,7 +42,6 @@ const musicList = [{
 
 
 const server = http.createServer(function(req, res) {
-    console.log(req.url)
     if (req.url == "/") {
         fs.readFile("./public/index.html", "utf8", function(err, data) {
             if (err) {
@@ -59,17 +59,71 @@ const server = http.createServer(function(req, res) {
     } else if (req.url.startsWith("/node_modules")) {
         fs.readFile("./" + req.url, function(err, data) {
             if (req.url.endsWith(".css")) {
-                console.log('css')
                 res.writeHead(200, { "Content-Type": "text/css" });
             } else if (req.url.endsWith(".js")) {
                 res.writeHead(200, { "Content-Type": "application/x-javascript" });
             }
             res.end(data);
         });
+    } else if (req.url == "/add") {
+        fs.readFile("./public/add.html", "utf8", function(err, data) {
+            if (err) {
+                console.log(err.message);
+                return;
+            }
+            res.writeHead(200, { "Content-Type": "text/html" });
+            res.end(data);
+        });
+    } else if (req.url == "/doAdd") {
+
+        getPostData(req, function(data) {
+            let dataObj = qs.parse(data);
+            let id = dataObj.id;
+            let name = dataObj.name;
+            let singer = dataObj.singer;
+            let isHightRate = dataObj.isHightRate;
+
+            if (id != '' && name != '' && singer != '' && isHightRate != '') {
+
+                let musicInfo = musicList.find(item => item.id == id);
+                if (musicInfo) {
+                    res.writeHead(200, { "Content-Type": "text/plain; charset=utf8" });
+                    res.write("音乐编号已经存在");
+                    return res.end();
+                }
+                musicList.push({
+                    id,
+                    name,
+                    singer,
+                    isHightRate
+                })
+                res.writeHead(200, { "Content-Type": "text/plain; charset=utf8" });
+                res.end("信息提交成功");
+
+            } else {
+                res.writeHead(200, { "Content-Type": "text/plain; charset=utf8" });
+                res.end("信息填写完整");
+            }
+
+        });
+
     }
 
 
 });
+
+
 server.listen(4000, '127.0.0.1', function() {
     console.log('listening 4000');
 })
+
+
+function getPostData(req, callback) {
+    let data = '';
+    req.on("data", function(chunk) {
+        data += chunk;
+    });
+    req.on("end", function() {
+        callback(data);
+    });
+}
